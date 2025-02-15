4d291583974806901675f468242e066f582199d0
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-02-16 01:54:07 +0600

    module 18.5 push.js uploaded

---
+++ Milestone 4/Module 18.5/push.js
@@ -0,0 +1,11 @@
+const age = []; // we can declare empty array
+const numbers = [12, 87, 98, 45];
+console.log(numbers);
+
+// we can push a single value in a array
+numbers.push(44);
+console.log(numbers);
+
+// we can push multiple values in a array
+numbers.push(98, 11, 9);
+console.log(numbers);
