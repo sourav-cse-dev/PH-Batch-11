086c90f45c5fc5a92b017526920e0a065272df43
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-02-16 00:34:37 +0600

    module 18.5 array.js uploaded

---
+++ Milestone 4/Module 18.5/array.js
@@ -0,0 +1,18 @@
+//We can declare 5 different variable to contain 5 values
+const num1 = 12;
+const num2 = 22;
+const num3 = 11;
+const num4 = 33;
+const num5 = 55;
+
+//We can declare a single array variable to contain more values
+const numbers = [12, 22, 11, 33, 55]; // elements of an array are seperated by comma
+console.log(numbers);
+
+//Arrays can contain strings
+const friends = ["Abul", "Babul", "Cabul", "Dabul", "Bulbul"];
+console.log(friends);
+
+//Arrays can contain mixed data types
+const mixedArray = [12, "Mirpur", true, 5.5];
+console.log(mixedArray);
