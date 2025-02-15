1e365e1cc1b0c5f03e7bc46a16b5c40b287c9e3f
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-02-16 01:38:04 +0600

    module 18.5 index.js uploaded

---
+++ Milestone 4/Module 18.5/index.js
@@ -0,0 +1,12 @@
+//Position of every elements of an array is called index and it starts from 0
+const numbers = [88, 77, 99, 421, 54, 63];
+
+//Get element by index
+console.log(numbers); // we will get full array
+console.log(numbers[2]); // we will get the element of 2nd index of the array
+const fourth = numbers[3]; // we can assign any indexed element of an array in a variable
+console.log(fourth);
+
+//Set or update element by index
+numbers[1] = 999;
+console.log(numbers);
