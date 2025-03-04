2c2955f04d8157ea673fd0f680242d77fb96a676
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-03-04 11:49:18 +0600

    some files are modified

---
+++ Milestone 5/Module 25/0-roadmap.js
@@ -0,0 +1,6 @@
+// JS দিয়ে ওয়েব পেইজ গুলোকে ডায়নামিক বা ইন্টারেক্টিভ বানানোর জন্য DOM বতাবহার করা হয়।
+// DOM(Document Object Model) এর মাধ্যমে আমরা HTML এবং CSS কে JS দিয়ে control করবো।
+// DOM, HTML কে একটি tree structure এ রিপ্রেজেন্ট করে, যেখানে HTML এর প্রতিটি element একেকটি object।
+// JS ব্যাবহার করে HTML এর DOM tree traverse(travel across) করতে পারবো,
+//          elements গুলোকে select করতে পারবো, এবং তাদের property গুলোকে পরিবর্তন করতে পারব।
+//
---
+++ Milestone 5/Module 25/js/1-dom.js
@@ -0,0 +1,4 @@
+console.log("Hello from JS");
+console.log(document);
+console.log(document.body.li);
+console.log(document.li);
