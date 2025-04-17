c20d240941b95828992c43aefbafb49d8ee7995c
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-04-17 16:52:12 +0600

    React Comceptual 1 -  part 3 completed

--- Milestone 7/Conceptual 1/Scratch/react.js
+++ Milestone 7/Conceptual 1/Scratch/react.js
@@ -23,6 +23,7 @@ ReactDOM.createRoot(document.getElementById("myDivRootShow")).render(
 //JSX element
 const myJSXElement = (
   <div>
+    <hr></hr>
     <p>Hello React create element from JSX</p>
   </div>
 );
@@ -36,26 +37,64 @@ const MyFirstFunction = (props) => {
   console.log("props", props);
   return (
     <div>
+      <hr></hr>
       <p>
         Hello React create element from JSX function, name:{" "}
         {props ? props.myName : "Guest"}
       </p>
-      {props ? props.children : "0"}
+      {/* {props ? props.children : "0"} */}
     </div>
   );
 };
 const Age = (props) => {
-    return <p>I am {props ? props.age : "0"} years old</p>;
-}
+  return <p>I am {props ? props.age : "0"} years old</p>;
+};
 console.log(MyFirstFunction());
 // ReactDOM.createRoot(document.getElementById("myDivRootShowJSXFunction")).render(
 //   MyFirstFunction()
 // );
 ReactDOM.createRoot(document.getElementById("myDivRootShowJSXFunction")).render(
-  <MyFirstFunction myName={"Labonno"}>
+  <>
+    <MyFirstFunction myName={"Labonno"}></MyFirstFunction>
     <Age age={25}></Age>
-  </MyFirstFunction>
-  
+    <Age age={26}></Age>
+    <Age age={27}></Age>
+    <Age age={28}></Age>
+  </>
 );
 
-//another react function & props
+// react useState
+const Counter = () => {
+  const [myCounter, setMyCounter] = React.useState(0);
+  console.log(myCounter);
+  console.log(setMyCounter);
+
+  function handleAdd(params) {
+    // const previousCount = document.getElementById("count").innerText;
+    // const parseNumber = parseInt(previousCount);
+    // document.getElementById("count").innerText = parseNumber + 1;
+    // console.log(previousCount);
+    // console.log(parseNumber);
+
+    setMyCounter(myCounter + 1);
+  }
+  return (
+    <div>
+      <hr></hr>
+      <p>
+        {" "}
+        Counter : <span id="count">{myCounter}</span>{" "}
+      </p>
+      {/* this will render the the counter component */}
+      <button onClick={handleAdd}>Click to add</button>
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("counter")).render(
+  <>
+    <Counter></Counter>
+    <Counter></Counter>
+    <Counter></Counter>
+  </>
+);
