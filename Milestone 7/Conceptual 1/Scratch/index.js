ca3b38a9a60f33a8eccf076beef51090cb5f88df
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-04-17 14:53:34 +0600

    React Comceptual 1 -  part 2 completed

--- Milestone 7/Conceptual 1/Scratch/index.js
+++ Milestone 7/Conceptual 1/Scratch/index.js
@@ -2,12 +2,16 @@ console.log("My JS");
 
 const root = document.getElementById("myDiv");
 let count = 0;
-root.innerHTML = `Hello JS ${count}`;
+root.innerHTML = `Counter: ${count}`;
 
 const myCustomElement = document.createElement("div");
 console.log(myCustomElement);
 
+function handleDecrement() {
+  count--;
+  root.innerHTML = `Counter: ${count}`;
+}
 function handleIncrement() {
   count++;
-  root.innerHTML = `Hello JS ${count}`;
+  root.innerHTML = `Counter: ${count}`;
 }
--- Milestone 7/Conceptual 1/Scratch/react.js
+++ Milestone 7/Conceptual 1/Scratch/react.js
@@ -1,9 +1,61 @@
 console.log(React);
 console.log(ReactDOM);
 
+//raw react
 const myReactElement = React.createElement(
   "div",
-  {name: "Sourav"},
-  "Hello React create element"
+  { name: "Sourav" },
+  //   "Hello React create element"
+  React.createElement(
+    "p",
+    { nestedName: "Chowdhury" },
+    "Hello React create element"
+  )
 );
+console.log(myReactElement);
+console.log("props", myReactElement.props);
 console.log(myReactElement.props.name);
+console.log(myReactElement.props.children.props.nestedName);
+ReactDOM.createRoot(document.getElementById("myDivRootShow")).render(
+  myReactElement
+);
+
+//JSX element
+const myJSXElement = (
+  <div>
+    <p>Hello React create element from JSX</p>
+  </div>
+);
+console.log(myJSXElement);
+ReactDOM.createRoot(document.getElementById("myDivRootShowJSX")).render(
+  myJSXElement
+);
+
+//react function & props
+const MyFirstFunction = (props) => {
+  console.log("props", props);
+  return (
+    <div>
+      <p>
+        Hello React create element from JSX function, name:{" "}
+        {props ? props.myName : "Guest"}
+      </p>
+      {props ? props.children : "0"}
+    </div>
+  );
+};
+const Age = (props) => {
+    return <p>I am {props ? props.age : "0"} years old</p>;
+}
+console.log(MyFirstFunction());
+// ReactDOM.createRoot(document.getElementById("myDivRootShowJSXFunction")).render(
+//   MyFirstFunction()
+// );
+ReactDOM.createRoot(document.getElementById("myDivRootShowJSXFunction")).render(
+  <MyFirstFunction myName={"Labonno"}>
+    <Age age={25}></Age>
+  </MyFirstFunction>
+  
+);
+
+//another react function & props
