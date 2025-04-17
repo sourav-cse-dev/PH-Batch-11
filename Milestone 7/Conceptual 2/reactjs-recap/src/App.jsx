f18eac3a6c9d20b6b23e8d552e88b68bfb4464b9
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-04-18 00:56:45 +0600

    React Conceptual 2 -  part 3 completed

--- Milestone 7/Conceptual 2/reactjs-recap/src/App.jsx
+++ Milestone 7/Conceptual 2/reactjs-recap/src/App.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+  // const handleClick = () => {
+  //   setCounter(counter + 1);
+  // }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.name.value);
+  };
+
+  const buttonRef = useRef(null);
+  const handleClick = () => {
+    alert("I got clicked!");
+  };
+
+  const buttonRef2 = useRef(null);
+  const handleClick2 = () => {
+    console.log(buttonRef2);
+  };
+
   return (
-    <div>
-      <h2 className="bg-teal-500">Hello, react JS</h2>
+    <div className="flex flex-col justify-center items-center m-10">
+      <p>Total Clicked {counter}</p>
+      <button
+        onClick={() => setCounter(counter + 1)}
+        className="btn btn-primary"
+      >
+        Click Me
+      </button>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input input-primary"
+          type="text"
+          // name="name"
+          ref={buttonRef2}
+          placeholder="Your Name"
+        />
+        <button
+          onClick={handleClick2}
+          type="submit"
+          className="btn btn-primary"
+        >
+          Submit
+        </button>
+      </form>
+
+      <button ref={buttonRef} onClick={handleClick} className="btn btn-primary">
+        Click me
+      </button>
     </div>
   );
 };
