5db3a105cc1e0e6feaf066892fa00185a11d936d
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-04-17 17:40:24 +0600

    React Comceptual 1 -  part 4 completed

---
+++ Milestone 7/Conceptual 1/my-react-app/src/components/Age.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Age = ({age}) => {
+  return <div> my age : {age}</div>;
+};
+
+export default Age;
---
+++ Milestone 7/Conceptual 1/my-react-app/src/components/Person.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Age from "./Age";
+
+const Person = ({name , age}) => {
+    // console.log(props);
+  return (
+    <>
+      <div className="border p-3 flex flex-col justify-center items-center">
+        I am {name},
+        <Age age={age}></Age>
+        <Age age={age}></Age>
+        <Age age={age}></Age>
+        <Age age={age}></Age>
+      </div>
+    </>
+  );
+};
+
+export default Person;
