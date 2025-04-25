8bf54cafb87173fac259f61513b92aaf8b1ee1e2
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-04-26 01:28:45 +0600

    learning DOM

--- Milestone 5/Module 25/0-roadmap.js
+++ Milestone 5/Module 25/0-roadmap.js
@@ -2,5 +2,5 @@
 // DOM(Document Object Model) এর মাধ্যমে আমরা HTML এবং CSS কে JS দিয়ে control করবো।
 // DOM, HTML কে একটি tree structure এ রিপ্রেজেন্ট করে, যেখানে HTML এর প্রতিটি element একেকটি object।
 // JS ব্যাবহার করে HTML এর DOM tree traverse(travel across) করতে পারবো,
-//          elements গুলোকে select করতে পারবো, এবং তাদের property গুলোকে পরিবর্তন করতে পারব।
-//
+// HTML elements গুলোকে select করতে পারবো, এবং তাদের property গুলোকে পরিবর্তন করতে পারব।
+// 
