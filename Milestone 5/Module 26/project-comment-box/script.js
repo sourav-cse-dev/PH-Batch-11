d086be0d8f2421c5aaa161efb0729aaf42284dc1
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-04-26 20:42:57 +0600

    project-comment-box v2

--- Milestone 5/Module 26/project-comment-box/script.js
+++ Milestone 5/Module 26/project-comment-box/script.js
@@ -2,21 +2,33 @@
 document.getElementById("btn-post-comment").addEventListener(
     'click',
     function(){
-        // 2. Get the text written in the comment text
+        // console.log("Post button clicked");
 
-        const commentContainer = document.getElementById("comment-container"); 
-    
+        // 2. get the text written in the comment textarea
         const textBox = document.getElementById("comment-text-box");
+        // console.log(textBox);
+
+        // 3. set the text to a new variable
         const newComment = textBox.value;
+        // console.log(newComment);
+
+        // 4. get the parent node
+        const commentContainer = document.getElementById("comment-container"); 
+        // console.log(commentContainer);
+
+        // 5.  create a new paragraph element to hold new comment
         const newParagraph = document.createElement('p');
+
+        // 6. set the text of the new paragraph to the new comment
         newParagraph.innerText = newComment;
+
+        // 7. set the class of the new paragraph to comment
         newParagraph.classList.add('comment')
 
-
+        // 8. append the new paragraph to the parent comment container
         commentContainer.appendChild(newParagraph);
 
+        // 9. clear the text box after clicking the post button
         textBox.value = '';
-
-
     }
 )
